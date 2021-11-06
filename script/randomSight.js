6eb55a6a34bccae9bb43ba14c73c795e76e4224a
Pedro PP <1qwertyuiop.wasd@gmail.com>
2021-11-06 23:32:37 +0000

    - melhoramentos nos tamanhos dos elementos. - resolvido bug ao escolher um exercicio, dropdown nao atualizava

--- script/randomSight.js
+++ script/randomSight.js
@@ -1,57 +1,60 @@
 
 
-var picsL1 = new Array("../../img/sight/L1/001.png","../../img/sight/L1/002.png","../../img/sight/L1/003.png","../../img/sight/L1/004.png","../../img/sight/L1/005.png","../../img/sight/L1/006.png","../../img/sight/L1/007.png","../../img/sight/L1/008.png","../../img/sight/L1/009.png","../../img/sight/L1/010.png","../../img/sight/L1/011.png","../../img/sight/L1/012.png");
-var sndsL1 = new Array("../../snd/sight/btSimple001.mp3","../../snd/sight/btSimple002.mp3","../../snd/sight/btSimple003.mp3","../../snd/sight/btSimple004.mp3","../../snd/sight/btSimple005.mp3","../../snd/sight/btSimple006.mp3","../../snd/sight/btSimple007.mp3","../../snd/sight/btSimple008.mp3","../../snd/sight/btSimple009.mp3","../../snd/sight/btSimple010.mp3","../../snd/sight/btSimple011.mp3","../../snd/sight/btSimple012.mp3");
+var picsL1 = new Array("../../img/sight/L1/001.png", "../../img/sight/L1/002.png", "../../img/sight/L1/003.png", "../../img/sight/L1/004.png", "../../img/sight/L1/005.png", "../../img/sight/L1/006.png", "../../img/sight/L1/007.png", "../../img/sight/L1/008.png", "../../img/sight/L1/009.png", "../../img/sight/L1/010.png", "../../img/sight/L1/011.png", "../../img/sight/L1/012.png");
+var sndsL1 = new Array("../../snd/sight/btSimple001.mp3", "../../snd/sight/btSimple002.mp3", "../../snd/sight/btSimple003.mp3", "../../snd/sight/btSimple004.mp3", "../../snd/sight/btSimple005.mp3", "../../snd/sight/btSimple006.mp3", "../../snd/sight/btSimple007.mp3", "../../snd/sight/btSimple008.mp3", "../../snd/sight/btSimple009.mp3", "../../snd/sight/btSimple010.mp3", "../../snd/sight/btSimple011.mp3", "../../snd/sight/btSimple012.mp3");
 
 var lastChoose = -1;
 
-function choosePic() 
+function chooseRandomPic() 
 {
     var randomNum = Math.floor(Math.random() * picsL1.length);
-    
-    if (lastChoose != -1)
-        {
-            while(randomNum == lastChoose)
-                {
-                    randomNum = Math.floor(Math.random() * picsL1.length);
-                }
+
+    if (lastChoose != -1) 
+    {
+        while (randomNum == lastChoose) {
+            randomNum = Math.floor(Math.random() * picsL1.length);
         }
-    
-    document.getElementById("sightIMG").src = picsL1[randomNum];
-    document.getElementById("sightAudio").src=sndsL1[randomNum];
-    
-    lastChoose = randomNum;
+    }
+
+    console.log(randomNum);
+
+   ChangeSheet(randomNum);
 }
 
-function Start()
+function Start() 
 {
     document.getElementById("sightIMG").src = picsL1[0];
-    document.getElementById("sightAudio").src=sndsL1[0];
-    
+    document.getElementById("sightAudio").src = sndsL1[0];
+
     var i = 0;
-    
+
     var ele = document.getElementById("sheetList");
-    
-    while(i < picsL1.length)
-        {
-            var sheet = document.createElement("option");
-            sheet.setAttribute("value",i);
-            if(i == 0)
-                {
-                    sheet.setAttribute("selected","");
-                }
-            sheet.textContent="Musica "+(i+1).toString();
-            ele.appendChild(sheet);
-            i++;
-        }
+
+    while (i < picsL1.length) 
+    {
+        var sheet = document.createElement("option");
+        sheet.setAttribute("value", i);
+        sheet.textContent = "Musica " + (i + 1).toString();
+        ele.appendChild(sheet);
+        i++;
+    }
 }
 
-function ChangeSheet(num)
+function ChangeSheet(num) 
 {
-    var value = num.value;
-    
+    var value = num;
+
+    if (isNaN(num))
+    {
+        value = num.value;
+    }
+
     lastChoose = value;
-    
+
+    document.getElementById("sheetList").selectedIndex = lastChoose;
+
+    console.log(lastChoose + " - "+value);
+
     document.getElementById("sightIMG").src = picsL1[value];
-    document.getElementById("sightAudio").src=sndsL1[value];
+    document.getElementById("sightAudio").src = sndsL1[value];
 }
